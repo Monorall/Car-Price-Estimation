39c1f2f62160a9f9a435be5c9977d1a82cbdc11f
Monoral <goldflesher@gmail.com>
2023-02-18 20:01:41 +0600

    Commit

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,47 @@
 class CarsController < ApplicationController
+  require 'open3'
+
+  def index
+
+  end
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(evaluate)
+
+    if @car.save
+      render :result
+    else
+      render :new
+    end
+  end
+
+  def evaluate
+    # получение входных параметров из формы
+    year = params[:year]
+    brand = params[:brand]
+    model = params[:model]
+    condition = params[:condition]
+    engine_volume = params[:engine_volume]
+    mileage = params[:mileage]
+    fuel_type = params[:fuel_type]
+
+    # выполнение скрипта python с передачей параметров
+    stdout, stderr, status = Open3.capture3("python3
+       #{Rails.root}/script/evaluate.py#{year} #{brand} #{model} #{condition} #{engine_volume} #{mileage} #{fuel_type}")
+
+    # парсинг выходных данных
+    result = stdout.strip.to_f
+
+    # обработка ошибок
+    if status.success?
+      @price = result
+      render :evaluate
+    else
+      flash[:error] = "Произошла ошибка: #{stderr}"
+      redirect_to root_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Defines the root path route ("/")
+  root "cars#index"
+
+  # Cars routes
+  get "/cars", to: "cars#index"
+  get "/cars/new", to: "cars#new"
+  post "/cars", to: "cars#create"
+  get "/cars/:id", to: "cars#show"
+  get "/cars/:id/edit", to: "cars#edit"
+  patch "/cars/:id", to: "cars#update"
+  delete "/cars/:id", to: "cars#destroy"
 end
