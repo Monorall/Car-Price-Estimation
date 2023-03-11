85f585873696813b766eff4bf5b48dae836e5db4
Gleb Tutik <tutikgv@gmail.com>
2023-03-11 12:15:00 +0600

    Model archetecture changes. Add optimal parameters

--- predictor/model.py
+++ predictor/model.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 
 
 class CarPricePredictor(nn.Module):
-    def __init__(self, input_size, output_size, num_layers, hidden_size, dropout_rate):
+    def __init__(self, input_size, output_size, num_layers=2, hidden_size=317, dropout_rate=0.32):
         super(CarPricePredictor, self).__init__()
 
         self.num_layers = num_layers
@@ -10,15 +10,13 @@ class CarPricePredictor(nn.Module):
         self.dropout_rate = dropout_rate
 
         self.relu = nn.ReLU()
-        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc = nn.Linear(input_size, hidden_size)
         self.dropout = nn.Dropout(p=dropout_rate)
-        self.hidden_layers = nn.ModuleList(
-            [nn.Linear(hidden_size, hidden_size) for _ in range(num_layers - 1)]
-        )
+        self.hidden_layers = nn.ModuleList([nn.Linear(hidden_size, hidden_size)] * (num_layers-1))
         self.out = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
-        x = self.fc1(x)
+        x = self.fc(x)
         x = self.relu(x)
         x = self.dropout(x)
 
@@ -30,24 +28,3 @@ class CarPricePredictor(nn.Module):
         x = self.out(x)
 
         return x
-
-
-
-# import config
-# import torch.nn as nn
-#
-#
-# class CarPricePredictor(nn.Module):
-#     def __init__(self, input_size=config.INPUT_FEATURES, output_size=config.OUT_FEATURES):
-#         super(CarPricePredictor, self).__init__()
-#
-#         self.fc1 = nn.Linear(input_size, 64)
-#         self.fc2 = nn.Linear(64, output_size)
-#         self.relu = nn.ReLU()
-#
-#     def forward(self, x):
-#         x = self.fc1(x)
-#         x = self.relu(x)
-#         x = self.fc2(x)
-#
-#         return x
